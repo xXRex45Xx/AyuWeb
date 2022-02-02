9313c2248d04893c63a1a8d891bcc23f9b8bda1c
xXRex45Xx <esrom.mulugeta@gmail.com>
2022-02-02 20:48:26 +0300

    Fixed Card Number Generator

--- utils/card-number-generator.js
+++ utils/card-number-generator.js
@@ -7,11 +7,11 @@ module.exports = function generateCardNo(lastCardNo) {
         firstLetter = String.fromCharCode(lastCardNo.charCodeAt(0) + 1)
         return firstLetter + "0000"
     }
-    else if(cardNums < 10)
+    else if(cardNums < 9)
         return lastCardNo.charAt(0) + "000" + (cardNums + 1).toString()
-    else if(cardNums < 100 && cardNums >= 10)
+    else if(cardNums < 99 && cardNums >= 9)
         return lastCardNo.charAt(0) + "00" + (cardNums + 1).toString()
-    else if(cardNums < 1000 && cardNums >= 100)
+    else if(cardNums < 999 && cardNums >= 99)
         return lastCardNo.charAt(0) + "0" + (cardNums + 1).toString()
     else 
         return lastCardNo.charAt(0) + (cardNums + 1).toString()
